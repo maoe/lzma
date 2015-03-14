6026476bfb6a9b751d0849392a02aeddd5355f24
Mitsutoshi Aoe <maoe@foldr.in>
2015-03-14 23:45:52 +0900

    Add new test suite

---
+++ tests/test-lzma.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE TemplateHaskell #-}
+import Control.Applicative
+
+import Test.Tasty
+import Test.Tasty.QuickCheck
+import Test.Tasty.TH
+import qualified Data.ByteString.Lazy as L
+
+import Codec.Compression.LZMA
+
+main :: IO ()
+main = $defaultMainGenerator
+
+prop_roundTripIdentity :: L.ByteString -> Bool
+prop_roundTripIdentity bytes = decompress (compress bytes) == bytes
+
+instance Arbitrary L.ByteString where
+  arbitrary = L.pack <$> arbitrary
+  shrink xs = L.pack <$> shrink (L.unpack xs)
